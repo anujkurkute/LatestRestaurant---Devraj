fe442e49011b5a16eefba9bffa5664934934ab96
7822029986 <mayuri2112003@gmail.com>
2024-06-04 09:45:47 +0530

    first commit

---
+++ vendor/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from . import views
+from accounts import views as AccountViews
+
+urlpatterns=[
+             path('', AccountViews.vendorDashboard, name='vendor'), 
+             path('profile/', views.vprofile, name='vprofile'),
+             path('menu-builder/',views.menu_builder,name='menu_builder'),
+             path('menu-builder/category/<int:pk>/', views.fooditems_by_category, name='fooditems_by_category'),
+             path('menu-builder/category/add/', views.add_category, name='add_category'),
+             path('menu-builder/category/edit/<int:pk>/', views.edit_category, name='edit_category'),
+             path('menu-builder/category/delete/<int:pk>/', views.delete_category, name='delete_category'),
+             path('menu-builder/food/add/', views.add_food, name='add_food'),
+             path('menu-builder/food/edit/<int:pk>/', views.edit_food, name='edit_food'),
+             path('menu-builder/food/delete/<int:pk>/', views.delete_food, name='delete_food'),
+             path('opening-hours/', views.opening_hours, name='opening_hours'),
+             path('opening-hours/add/', views.add_opening_hours, name='add_opening_hours'),
+             path('opening-hours/remove/<int:pk>/', views.remove_opening_hours, name='remove_opening_hours'),
+             ]
